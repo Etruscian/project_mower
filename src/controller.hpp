63b83c08b344df3febb543068bf4a8b1ce0539e8
SamStaps <sjstaps@gmail.com>
2018-05-08 16:45:37 +0200

    Added fuzzy throttle control and speed estimation

--- src/controller.hpp
+++ src/controller.hpp
@@ -15,11 +15,19 @@ class Controller
 
     void update(float throttle, float direction)
     {
-        _steeringGain = 1 - 0.8*throttle**2;
+        filterThrottle = 0.995*filterThrottle + 0.005*throttle;     // filtered throttle as speed estimate, 100Hz, in 2s op topSpeed
+        _steeringGain = 1 - 0.8*filterThrottle**2;        // 0.8 value is arbitrary, _steeringGain [0,1]
+        uSteer = direction * _steeringGain;         // direction [-1,1]
+        x1 = 0.2;                                   // input = 0.2
+        y1 = 0.35;                                  // output = 0.35
+        if( throttle <= x1)                         // fuzzy throttle control
+            uForward = y1/x1 *throttle;             // uForward [0, 1]
+        else
+            uForward = y1 + (1-y1)/(1-x1)*(throttle-x1);
         if( _leftMotor._isEnabled)
-            _leftMotor.setSpeed(throttle + direction * _steeringGain);
+            _leftMotor.setSpeed(uForward + uSteer);      // setSpeed [0, 1] 
         if( _rightMotor._isEnabled)
-            _rightMotor.setSpeed(throttle - direction * _steeringGain);
+            _rightMotor.setSpeed(uForward - uSteer);
     };
 
     void enableMotors(bool toggle)
@@ -32,6 +40,7 @@ class Controller
     Motor _leftMotor;
     Motor _rightMotor;
     float _steeringGain ; //= 0.3;
+    float x1, y1, uForward, uSteer, filterThrottle;
 };
 
 #endif
