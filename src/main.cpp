2302faa06e3412d53502977f209f0b9ceea8d9d4
sanswart@gmail.com <sanswart@gmail.com>
2018-05-08 19:13:27 +0200

    replaced library with ported version of transmitter library

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,62 @@
 #include <Arduino.h>
-#include "RF24.h"
+#include <SPI.h>
+// #include "nRF24L01P.hpp"
 #include <controller.hpp>
 
-uint8_t address[] = {0x00, 0xFD, 0xEA, 0xDB, 0xEE};
-RF24 radio(49,53);
+// nRF24L01P radio(49,47);
 float i = 0;
 Controller controller;
 
 void interruptRoutine(void){
     Serial.println("hi");
+    // while(true){Serial.println(radio.get_status());}
 }
 
 void setup() {
-    attachInterrupt(digitalPinToInterrupt(20), interruptRoutine, FALLING);
-    radio.begin();
     Serial.begin(9600);
+    // SPI.begin();
+    Serial.println("h i");
+    // radio.initialize();
+    // attachInterrupt(digitalPinToInterrupt(20), interruptRoutine, FALLING);
     Serial.println("Motor off");
-    Serial.println(radio.get_status());
-    radio.openReadingPipe(0, 0x00FDEADBEE);
-    radio.setCRCLength(RF24_CRC_16);
-    radio.setDataRate(RF24_250KBPS);
-    radio.setPayloadSize(17);
-    radio.setChannel(101);
-    radio.setAutoAck(0,true);
-    radio.startListening();
+    // Serial.println(radio.getStatusRegister());
+    // radio.powerUp();
+    // radio.setRfFrequency(2400 + 101);
+    // radio.setTransferSize(17);
+    // radio.setCrcWidth(16);
+    // radio.setTxAddress(0x007DEADBEE);
+    // radio.setRxAddress(0x007DEADBEE);
+    // radio.enableAutoAcknowledge(NRF24L01P_PIPE_P0);
+    // radio.setAirDataRate(NRF24L01P_DATARATE_250_KBPS);
+    // radio.enableAutoRetransmit(1000, 3);
+    // radio.setReceiveMode();
     }
 
 void loop() {
+    char data[17];
+    // radio.write(0,data, 17);
     // Serial.println(radio.testRPD());
-    // if (Serial.available()){
-    //     char input = Serial.read();
-    //     if (input == 's'){
-    //         if (i>0){
-    //             i = i-5;
-    //             controller.update(i,0);
-    //             Serial.println((int)(i));
-    //         }
-    //     } else if (input == 'w'){
-    //         if (i<100){
-    //             i = i+5;
-    //             controller.update(i,0);
-    //             Serial.println((int)(i));
-    //         }
-    //     } else if (input == 'i'){
-    //         controller.enableMotors(true);
-    //         Serial.println("Motor on");
-    //     } else if (input == 'u'){
-    //         controller.enableMotors(false);
-    //         Serial.println("Motor off");
-    //     }
-    // }
-    // delay(10);
+    if (Serial.available()){
+        char input = Serial.read();
+        if (input == 's'){
+            if (i>0){
+                i = i-5;
+                controller.update(i,0);
+                Serial.println((int)(i));
+            }
+        } else if (input == 'w'){
+            if (i<100){
+                i = i+5;
+                controller.update(i,0);
+                Serial.println((int)(i));
+            }
+        } else if (input == 'i'){
+            controller.enableMotors(true);
+            Serial.println("Motor on");
+        } else if (input == 'u'){
+            controller.enableMotors(false);
+            Serial.println("Motor off");
+        }
+    }
+    delay(10);
 }
